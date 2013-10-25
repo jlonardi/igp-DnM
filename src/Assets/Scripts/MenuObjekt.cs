fd75a8de947ca049eb267cd65fe143a9bfa35401
joaijala <johanna.aijala@helsinki.fi>
2013-10-25 12:43:37 +0300

    menu

--- src/Assets/Scripts/MenuObjekt.cs
+++ src/Assets/Scripts/MenuObjekt.cs
@@ -1,15 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MenuObjekt : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
+public class MenuObjekt : MonoBehaviour 
+{
+	public bool isQuit =false; //tells what will happen when pressing buttons
+	void OnMouseEnter()
+	{
+		renderer.material.color=Color.red;	//when mouse is on text it will become red
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void OnMouseExit()
+	{
+		renderer.material.color= Color.white; //when mouse leaves the text it will go back to white
 	}
+	void OnMouseDown() //if mouse is pressed on text somthing happens
+	{
+		if(isQuit)
+		{
+			Application.Quit(); //if quit game button is pressed game quits
+		}
+		else
+		{
+			Application.LoadLevel(1);  // loads main level
+		}
+	}
+				
 }
+
