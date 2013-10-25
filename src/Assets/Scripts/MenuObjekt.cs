1d7e8c3071cb681fb5f098b80590a723cb3b7e6c
joaijala <johanna.aijala@helsinki.fi>
2013-10-25 12:57:51 +0300

    menu

--- src/Assets/Scripts/MenuObjekt.cs
+++ src/Assets/Scripts/MenuObjekt.cs
@@ -3,14 +3,19 @@ using System.Collections;
 
 public class MenuObjekt : MonoBehaviour 
 {
+	
+	void Start()
+	{
+		renderer.material.color=Color.red; //To get the text to be red in the beginning
+	}
 	public bool isQuit =false; //tells what will happen when pressing buttons
 	void OnMouseEnter()
 	{
-		renderer.material.color=Color.red;	//when mouse is on text it will become red
+		renderer.material.color=Color.white;	//when mouse is on text it will become white
 	}
 	void OnMouseExit()
 	{
-		renderer.material.color= Color.white; //when mouse leaves the text it will go back to white
+		renderer.material.color= Color.red; //when mouse leaves the text it will go back to red
 	}
 	void OnMouseDown() //if mouse is pressed on text somthing happens
 	{
