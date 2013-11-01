5573525a94bbda515c5b49120402fb1d0fdb0ea6
Jarno Lonardi <jarno.lonardi@helsinki.fi>
2013-11-01 15:49:58 +0200

    pakotettu commit

---
+++ src/Assets/Scripts/Game logics/EnemySpawnManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawnManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		Terrain terrain = Terrain.activeTerrain;
+		//Debug.Log("terrain size = " + terrain.terrainData.size);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
