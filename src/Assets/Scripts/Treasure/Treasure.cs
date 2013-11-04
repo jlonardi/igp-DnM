6661e3b61ed64eeaf877413f952c3a9517a57ca7
tokri <tokri@sci.fi>
2013-11-04 13:48:25 +0200

    arkku asetellaan maaston mukaan ja aukeaa tiputettaessa

--- src/Assets/Scripts/Treasure/Treasure.cs
+++ src/Assets/Scripts/Treasure/Treasure.cs
@@ -2,17 +2,22 @@
 using System.Collections;
 
 public class Treasure : MonoBehaviour {
+	public bool onGround;
 	private Hud hud;
 	private int treasureAmount = 100;
-	public bool onGround;
-
+	private Terrain terrain;
+	private Animator animator;
+	private SimpleSmoothMouseLook mouseLook;
+	
 	void Start () {
+		animator = GetComponent<Animator>();
+		animator.speed = 4;	// animation playback speed
+		animator.SetBool("onGround",false);
+		
+		terrain = GameObject.Find("Terrain").GetComponent<Terrain>();
 		hud = GameObject.Find("Game Manager").GetComponent<Hud>();
-	
-	}
-	
-	void Update () {
 		hud.setTreasure(treasureAmount);
+		mouseLook = GameObject.FindObjectOfType(typeof(SimpleSmoothMouseLook)) as SimpleSmoothMouseLook;
 	}
 	
 	public int Loot(int lootAmount){
@@ -27,14 +32,27 @@ public class Treasure : MonoBehaviour {
 			lootAmount = treasureAmount;
 			treasureAmount = 0;
 		}
+		hud.setTreasure(treasureAmount);
 		return lootAmount;
 	}
 	
 	public void SetTreasureOnGround(){
 		GameObject treasureOnScene = GameObject.Find("TreasureOnGround");
-		this.transform.parent = treasureOnScene.transform;
+		this.transform.parent = treasureOnScene.transform;	
 		this.onGround = true;	
 		hud.setTreasureOnGround();
+
+		// align treasure on terrain
+	    transform.position = new Vector3(transform.position.x, transform.position.y + 10, transform.position.z); //first move up to make sure it doesn't go through terrain
+ 		RaycastHit hit = new RaycastHit();		
+		if (Physics.Raycast(transform.position, -Vector3.up, out hit)){
+			float yValue = terrain.SampleHeight(transform.position);
+		    transform.position = new Vector3(transform.position.x, yValue + 0.01f, transform.position.z); // align just above terrain
+ 			Vector3 proj = transform.forward - (Vector3.Dot(transform.forward, hit.normal)) * hit.normal;
+			transform.rotation = Quaternion.LookRotation(proj, hit.normal);
+		}
+		animator.SetBool("onGround",true);
+		mouseLook.clampInDegrees = new Vector2(360, 180);
 	}
 	
 	public bool OnGround(){
