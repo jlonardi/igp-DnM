50e4b633d85f034fe4077e6407bf94ca42ad5952
tokri <tokri@sci.fi>
2013-11-17 21:19:05 +0200

    Jos maksimimäärä ragdolleja täynnä, poistetaan vanhimmasta päästä se, joka ei ole renderoituna pelaajan näkymässä, jos tämä ei ole mahdollista, poistetaan ragdoll joka on pelaajasta kauimpana

--- src/Assets/Scripts/Enemies/RagdollManager.cs
+++ src/Assets/Scripts/Enemies/RagdollManager.cs
@@ -8,23 +8,50 @@ public class RagdollManager : MonoBehaviour {
 
 	public GameObject orcRagdollPrefab;
 	public int maxRagdolls = 20;
+	public GameObject player;
 
 	private List<GameObject> bodies = new List<GameObject>();
 
-	void Awake()
-	{
+	void Awake() {
 		RagdollManager.instance = this;
 	}	
+
+	void Start() {
+		player = GameObject.Find("Player");
+	}
 	
 	public GameObject MakeRagdoll(EnemyType enemyType, GameObject enemyObject, bool copyPose){		
 		GameObject ragdollPrefab;
 
-		// if too many dead bodies, remove the oldest
+		// if too many dead bodies, remove the oldest ragdoll which isn't on rendered on current view
 		if (bodies.Count >= maxRagdolls){
-			GameObject killObj = bodies[0];
-			bodies.RemoveAt(0);
+			foreach (GameObject body in bodies){
+				Renderer bodyRenderer = body.GetComponentInChildren<Renderer>();
+				if (bodyRenderer.isVisible == false){
+					bodies.Remove(body);
+					Destroy(body);
+					break;
+				}
+			}
+		}
+
+		// if previous way wasn't possible, remove the body furthest away
+		if (bodies.Count >= maxRagdolls){
+			int farEnemyIndex = 0;
+			float farEnemyDistance = 0f;
+			float enemyDistance;
+			for (int i=0; i< bodies.Count; i++){
+				enemyDistance = Vector3.Distance(bodies[i].transform.position, player.transform.position);
+				if (enemyDistance > farEnemyDistance){
+					farEnemyDistance = enemyDistance;
+					farEnemyIndex = i;
+				}
+			}
+			GameObject killObj = bodies[farEnemyIndex];
+			bodies.RemoveAt(farEnemyIndex);
 			Destroy(killObj);
 		}
+
 
 		// select correct ragdoll prefab by enemytype
 		switch(enemyType) {
