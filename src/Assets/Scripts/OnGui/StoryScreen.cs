1880e8783d83c5998200028e2d3b08bf6bb21e66
tokri <tokri@sci.fi>
2013-12-04 18:01:11 +0200

    alkutarinan kuvat prefabiin

--- src/Assets/Scripts/OnGui/StoryScreen.cs
+++ src/Assets/Scripts/OnGui/StoryScreen.cs
@@ -1,18 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[System.Serializable]
 public class StoryScreen {
 	private GameManager game;
 	private OnGuiManager gui;
+	public Texture2D[] storyImages;
+
 	private int centerX;
 	private int centerY;
 	private int storySlide = 1;
+	private Rect imagePosition;
 
 	public void Initialize(){
 		game = GameManager.instance;
 		gui = OnGuiManager.instance;
 		centerX = gui.GetCenterX();
 		centerY = gui.GetCenterY();
+
 	}
 
 	// Show() gets called from OnGuiManager
@@ -44,16 +49,18 @@ public class StoryScreen {
 			GUI.Box(new Rect(centerX-138, centerY-100,275,250),"Slide 5", myStyle);
 			GUI.Label(new Rect(centerX-100, centerY-150,275,250), "nothing yet");
 			break;
-			break;
 		}
+		imagePosition = new Rect(0, 0, 1920, 1080);
+		GUI.DrawTexture(imagePosition, storyImages[storySlide-1]);
 
-		GUILayout.BeginArea(new Rect(centerX-50, centerY+100,100,200));
+		GUILayout.BeginArea(new Rect(gui.GetWidth()-200, 1000,100,200));
 		if (storySlide<5){
 			if(GUILayout.Button ("Next"))
 			{
 				storySlide++;
 			}
 		} else if(GUILayout.Button ("Start game")) {
+			storySlide=1;
 			//loads first level
 			GameManager.instance.levelState = LevelState.LOADING_NEWGAME;
 			Application.LoadLevel("GameLevel");
