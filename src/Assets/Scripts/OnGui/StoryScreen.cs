9b7eae89e6a8aaf3b30b481a3235917a9348c33f
tokri <tokri@sci.fi>
2013-12-05 14:53:18 +0200

    pieni päivitys tarinaan

--- src/Assets/Scripts/OnGui/StoryScreen.cs
+++ src/Assets/Scripts/OnGui/StoryScreen.cs
@@ -27,8 +27,6 @@ public class StoryScreen {
 			Initialize();
 		}
 
-		GUIStyle myStyle = new GUIStyle("Box");
-		myStyle.fontSize=30;
 		imagePosition = new Rect(0, 0, 1920, 1080);
 		
 		//if already at last slide, don't check the slide counter
@@ -38,23 +36,23 @@ public class StoryScreen {
 			GUI.DrawTexture(imagePosition, storyImages[storySlide-1]);
 			switch (storySlide){
 			case 1:
-				GUI.Box(new Rect(centerX-138, centerY-100,275,250),"Slide 1", myStyle);
+				GUI.Box(new Rect(centerX-138, centerY-100,275,250),"Slide 1");
 				GUI.Label(new Rect(centerX-100, centerY-150,275,250), "nothing yet");
 				break;
 			case 2:
-				GUI.Box(new Rect(centerX-138, centerY-100,275,250),"Slide 2", myStyle);
+				GUI.Box(new Rect(centerX-138, centerY-100,275,250),"Slide 2");
 				GUI.Label(new Rect(centerX-100, centerY-150,275,250), "nothing yet");
 				break;
 			case 3:
-				GUI.Box(new Rect(centerX-138, centerY-100,275,250),"Slide 3", myStyle);
+				GUI.Box(new Rect(centerX-138, centerY-100,275,250),"Slide 3");
 				GUI.Label(new Rect(centerX-100, centerY-150,275,250), "nothing yet");
 				break;
 			case 4:
-				GUI.Box(new Rect(centerX-138, centerY-100,275,250),"Slide 4", myStyle);
+				GUI.Box(new Rect(centerX-138, centerY-100,275,250),"Slide 4");
 				GUI.Label(new Rect(centerX-100, centerY-150,275,250), "nothing yet");
 				break;
 			case 5:
-				GUI.Box(new Rect(centerX-138, centerY-100,275,250),"Slide 5", myStyle);
+				GUI.Box(new Rect(centerX-138, centerY-100,275,250),"Slide 5");
 				GUI.Label(new Rect(centerX-100, centerY-150,275,250), "nothing yet");
 				break;
 			}
@@ -67,7 +65,7 @@ public class StoryScreen {
 				storySlide++;
 			}
 		} else if(storySlide == 5){
-			if(GUILayout.Button ("Start game")) {
+			if(GUILayout.Button ("Start")) {
 				//loads first level
 				GameManager.instance.levelState = LevelState.LOADING_NEWGAME;
 				Application.LoadLevel("GameLevel");
