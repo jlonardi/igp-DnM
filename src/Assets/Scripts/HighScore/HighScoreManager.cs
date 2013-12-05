a3c72a7ae6ed83caea36117059f8eeb1dd7b3305
joaijala <johanna.aijala@helsinki.fi>
2013-12-05 22:07:36 +0200

    high scoren scriptit

---
+++ src/Assets/Scripts/HighScore/HighScoreManager.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+using System;
+using System.Runtime.Serialization.Formatters.Binary; 
+
+public class HighScoreManager : MonoBehaviour {
+
+
+	private List<Score> scores;
+	private string saveDirectory;
+
+	void Start(){
+
+		scores = new List<Score>();
+		findSaveDirectory();
+		loadScoresFromFile();
+	
+	}
+	//sorts the score array
+	private void sort(){
+		scores.Sort();
+	}
+	//returns the smallest score if there is less than 10 scores (max amount of scores) it returns 0
+	public int getSmallestScore() {
+		if (scores.Count<10) {
+			return 0;
+		}
+		else {
+			return scores[9].getScore();
+		}
+	}
+	//returns the score list
+	public List<Score> getScores() {
+		return scores;
+	}
+
+
+
+
+	//tries to find the directory where to save, creates new one if it doesn't excist
+	private void findSaveDirectory(){
+		string docsDirectory;
+		try {
+			docsDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+		} catch (Exception){
+			//if can't get My Documents-folder, put saves in <current folder>/highScore/
+			docsDirectory = "highScore"; 
+		}
+		
+		saveDirectory = docsDirectory + "\\Dragons and Miniguns";
+		
+		if (!Directory.Exists(saveDirectory))
+			Directory.CreateDirectory(saveDirectory);
+	}
+
+
+	//loads the old scores from the score file
+	private void loadScoresFromFile(){
+		string filePath = saveDirectory + "\\highScore"+".dat";
+		Stream stream = File.Open(filePath, FileMode.OpenOrCreate);
+		BinaryFormatter bformatter = new BinaryFormatter();
+		scores=(List<Score>)bformatter.Deserialize(stream);
+		stream.Close();
+	}
+	//uppdates the score file
+	private void uppdateScoreFile(){
+		string filePath = saveDirectory + "\\highScore"+".dat";
+		Stream stream = File.Open(filePath, FileMode.Create);
+		BinaryFormatter bformatter = new BinaryFormatter();
+		bformatter.Serialize(stream, scores);
+		stream.Close();
+	}
+
+	
+
+
+}
---
+++ src/Assets/Scripts/HighScore/Score.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Runtime.Serialization;
+
+[Serializable()] public class
+Score : IComparable<Score>{
+	private int score;
+	private string playerName;
+	// Use this for initialization
+	void Start (int score, string name) {
+		this.score=score;
+		this.playerName=name;
+	}
+	
+	public int getScore(){
+		return this.score;
+	}
+	
+	public string getName(){
+		return this.playerName;
+	}
+
+
+	public int CompareTo(Score score1) {
+		if(this.score==score1.getScore()){
+			return 0;
+		}
+		else if(this.score>score1.getScore()){
+			return 1;
+		}
+		else {
+			return -1;
+		}
+	}
+}
