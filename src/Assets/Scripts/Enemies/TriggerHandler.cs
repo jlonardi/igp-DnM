f53a8239a5c5d2a17cd5b8f7e388526ba4f6802c
Jarno Lonardi <jarno.lonardi@helsinki.fi>
2013-12-07 01:47:47 +0200

    lairin triggereille luotu runko

---
+++ src/Assets/Scripts/Enemies/TriggerHandler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerHandler : MonoBehaviour {
+
+	public bool playerInsideLair = false;
+
+	public void handleTrigger (Collider other) {
+	
+		if(other.tag == "Player") {
+			if(playerInsideLair) {
+				playerInsideLair = false;
+			} else {
+				playerInsideLair = true;
+			}
+			Debug.Log("Player hit the trigger");
+		}
+	}
+}
