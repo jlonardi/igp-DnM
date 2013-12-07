45ddab94ba24902c761cede1310358838b550c0d
Jarno Lonardi <jarno.lonardi@helsinki.fi>
2013-12-07 16:03:41 +0200

    lohikäärmeen toiminnallisuutta muokattu

---
+++ src/Assets/Scripts/Enemies/Dragon/EnterTrigger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnterTrigger : MonoBehaviour {
+
+	void OnTriggerEnter (Collider other) {
+		
+		transform.parent.GetComponent<TriggerHandler>().handleEnterTrigger(other);
+		
+	}
+}
---
+++ src/Assets/Scripts/Enemies/Dragon/TriggerHandler.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerHandler : MonoBehaviour {
+
+	public bool playerInsideLair = false;
+
+	public Dragon dragon;
+	public bool dragonHasAggroOnPlayer = false;
+	public bool playerInsideTheLair = false;
+	public bool playerKilled = false;
+
+	public void handleKillTrigger (Collider other) {
+	
+		if(other.tag == "Player") {
+			if(dragonHasAggroOnPlayer) {
+				if(!playerKilled) {
+					dragon.killPlayer();
+					playerKilled = true;
+				}
+			}
+		}
+	}
+
+	public void handleAggroTrigger (Collider other) {
+		
+		if(other.tag == "Player") {
+			if(!dragonHasAggroOnPlayer) {
+				dragonHasAggroOnPlayer = true;
+				dragon.flyBackToLair();
+				Debug.Log("Player aggroed the dragon");
+			}
+		}
+	}
+
+	public void handleEnterTrigger (Collider other) {
+		
+		if(other.tag == "Player") {
+			if(playerInsideTheLair) {
+				playerInsideTheLair = false;
+				Debug.Log("Player exits the lair");
+			} else {
+				playerInsideTheLair = true;
+				Debug.Log("Player entered the lair");
+			}
+		}
+	}
+}
